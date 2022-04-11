8b0167f5edfdcaaa4b8afdf09d7c42eae06e0b32
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2022-04-10 23:41:18 -0600

    Regresando a 16c6b3a, otra vez

--- server/auth/authRouter.js
+++ server/auth/authRouter.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const passport = require('passport');
+const { checkAuthenticated, checkNotAuthenticated } = require('./middleware');
 
 const router = express.Router();
+const initPassport = require('./passport-config');
+initPassport(passport);
 
 
-router.post('/login', (req, res) => { });
+router.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/homepage',
+  failureRedirect: '/api/auth/login',
+  failureFlash: true
+}));
 
-router.post('/logout', (req, res) => {
+router.post('/logout', checkAuthenticated, (req, res) => {
   req.logOut();
   res.redirect('/');
 })
